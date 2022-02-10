b5e20cb1b19e88b3cec9f5188165f5f0c00583ae
SammeH0312 <97672843+SammeH0312@users.noreply.github.com>
2022-02-10 13:32:09 +0100

    Add files via upload

--- verynicekod.py
+++ verynicekod.py
@@ -1,4 +1,23 @@
-x = input("Vad tränade du?")
-y = input("Hur lång tid körde du?")
-z = input("Ökade du eller sänkte du vikten du tränade med?")
+import calendar
+from pyexpat import native_encoding  
+
+dagens_pass = []
+exakt_pass = []
+
+x = input("Vilken datum är det idag?")
+y = input("Vad har du tränat?")
+z = input("Hur tungt tränade du?")
+
+
+def traningspassfunc():
+    traningspass = []
+    muskelgrupper = open("Tillämpad Programmering/träningspass.txt", "r", encoding="utf8")
+    traningspass.append(muskelgrupper)
+    return traningspass
+
+def tyngdfunc():
+    tyngd = []
+    tyngdlista = open("Tillämpad Programmering/tyngd.txt", "r", encoding="utf8")
+    tyngd.append(tyngdlista)
+    return tyngd
 
